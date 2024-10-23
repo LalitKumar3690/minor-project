bf3bd7b722caa32ce71b4b33f69cf55551cc84fb
LalitKumar3690 <lalitkuamrkumar369@gmail.com>
2024-10-23 12:07:04 +0530

    some changes

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,9 @@ const express = require('express')
 const {Server: SocketServer} = require('socket.io')
 const os = require('os');
 const pty = require('node-pty');
+const cors = require('cors')
+const fs = require('fs/promises')
+const path = require('path')
 
 const shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
 
@@ -10,7 +13,7 @@ const ptyProcess = pty.spawn(shell, [], {
   name: 'xterm-color',
   cols: 80,
   rows: 30,
-  cwd: process.env.INIT_CWD,
+  cwd: process.env.INIT_CWD + '/user',
   env: process.env
 });
 
@@ -19,6 +22,7 @@ const server = http.createServer(app);
 const io = new SocketServer({
     cors: '*'
 })
+app.use(cors())
 
 io.attach(server);
 
@@ -34,4 +38,32 @@ io.on('connection', (socket) =>{
     })
 })
 
+app.get('/files', async (req, res) => {
+    const fileTree =  await generateFileTree('./user')
+    return res.json({tree: fileTree})
+})
+
 server.listen(9000, ()=> console.log('hogya chalo server'))
+
+async function generateFileTree(directory){
+    const tree = {}
+
+    async function buildTree(currentDir, currenttree) {
+        const files = await fs.readdir(currentDir)
+
+        for (const file of files){
+            const filePath = path.join(currentDir, file)
+            const stat = await fs.stat(filePath)
+
+            if(stat.isDirectory()){
+                currenttree[free] = {}
+                await buildTree(filePath,currenttree[file])
+            } else {
+                currenttree[file] = null
+            }
+        }
+        
+    }
+    await buildTree(directory, tree);
+    return tree;
+} 
